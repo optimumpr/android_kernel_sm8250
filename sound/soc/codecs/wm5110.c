cf283d6f0041311f006e33704f5a2a19ed4f09f1
optimumpro <1optimizerpro@gmail.com>
2022-11-30 17:07:04 -0500

    upstream commits to .267

--- sound/soc/codecs/wm5110.c
+++ sound/soc/codecs/wm5110.c
@@ -2448,6 +2448,8 @@ static int wm5110_probe(struct platform_device *pdev)
 	for (i = 0; i < ARRAY_SIZE(wm5110_digital_vu); i++)
 		regmap_update_bits(arizona->regmap, wm5110_digital_vu[i],
 				   WM5110_DIG_VU, WM5110_DIG_VU);
+	pm_runtime_enable(&pdev->dev);
++	pm_runtime_idle(&pdev->dev);
 
 	ret = arizona_request_irq(arizona, ARIZONA_IRQ_DSP_IRQ1,
 				  "ADSP2 Compressed IRQ", wm5110_adsp2_irq,
@@ -2480,9 +2482,6 @@ static int wm5110_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to register component: %d\n", ret);
 		goto err_spk_irqs;
 	}
-
-	pm_runtime_enable(&pdev->dev);
-	pm_runtime_idle(&pdev->dev);
 
 	return ret;
 
