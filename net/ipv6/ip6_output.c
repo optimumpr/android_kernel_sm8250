62aab6d8ddf848c9d972353140785178bb15811f
Eric Dumazet <edumazet@google.com>
2022-12-15 15:09:27 -0500

    ipv6: avoid use-after-free in ip6_fragment

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -733,6 +733,8 @@ int ip6_fragment(struct net *net, struct sock *sk, struct sk_buff *skb,
 		skb->len = first_len;
 		ipv6_hdr(skb)->payload_len = htons(first_len -
 						   sizeof(struct ipv6hdr));
+		/* We prevent @rt from being freed. */
+		rcu_read_lock();
 
 		for (;;) {
 			/* Prepare header of the next frame,
@@ -776,6 +778,7 @@ int ip6_fragment(struct net *net, struct sock *sk, struct sk_buff *skb,
 		if (err == 0) {
 			IP6_INC_STATS(net, ip6_dst_idev(&rt->dst),
 				      IPSTATS_MIB_FRAGOKS);
+			rcu_read_unlock();
 			return 0;
 		}
 
@@ -783,6 +786,7 @@ int ip6_fragment(struct net *net, struct sock *sk, struct sk_buff *skb,
 
 		IP6_INC_STATS(net, ip6_dst_idev(&rt->dst),
 			      IPSTATS_MIB_FRAGFAILS);
+		rcu_read_unlock();
 		return err;
 
 slow_path_clean:
